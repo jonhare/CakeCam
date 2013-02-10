2d2c2623b1b35b1d41244798ad9ae88055668ace
Rikki Prince <rikki@prince.io>
2013-02-10 23:17:52 +0000

    Moved configuration to an external file, and added ability to tweet updates.
    Used Java .properties file to store configuration of the SMTP server, mailto
    address and cake location details, for easy setup at other sites.
    
    Added support for tweeting details of cakes using twitter4j.

--- src/main/java/pm5/CakeCam.java
+++ src/main/java/pm5/CakeCam.java
@@ -1,8 +1,8 @@
 package pm5;
-import java.io.BufferedReader;
+
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashSet;
@@ -29,6 +29,8 @@ import org.openimaj.video.VideoDisplay;
 import org.openimaj.video.VideoDisplayListener;
 import org.openimaj.video.capture.VideoCapture;
 
+import twitter4j.*;
+
 import javax.imageio.stream.FileImageInputStream;
 import javax.imageio.ImageIO;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
@@ -37,13 +39,39 @@ import com.google.zxing.BinaryBitmap;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.multi.qrcode.QRCodeMultiReader;
 import com.google.zxing.Result;
-
+/**
+ * TODO
+ * [ ] Store image in memory, rather than temporary file.
+ * [ ] Some solution to not send e-mail when there is no food.
+ * [x] Load mail host from properties file
+ * [x] Load mail addresses from properties file
+ * [x] Load food location from properties file
+ * [x] Load food names from properties file
+ */
 public class CakeCam 
 {
     public static void main(String[] args) 
     {
         //Testing on windows
         final String PATH = System.getProperty("java.io.tmpdir")+"CakeCam/";
+        //final String emailFromAddress = "cakecam@ecs.soton.ac.uk";
+        //final String emailToAddress = "rfp@ecs.soton.ac.uk";
+        
+        // Load cakecam.properties
+        final Properties ccProperties = new Properties();
+        try
+		{
+			ccProperties.load(new FileInputStream("cakecam.properties"));
+		}
+		catch (FileNotFoundException fnfe)
+		{
+			System.err.println("Could not find cakecam.properties. Message: "+fnfe.getMessage());
+		}
+		catch (IOException ioe)
+		{
+			System.err.println("Failed to load cakecam.properties. Message: "+ioe.getMessage());
+		}
+        
         VideoCapture vc = null;
         try 
         {
@@ -68,15 +96,23 @@ public class CakeCam
                 frame = ColourSpace.RGB.convertFromRGB(frame);
                 DisplayUtilities.displayName(frame, "CakeCam");
                 
+                // Delay to allow user to check photo?
                 if(frameCount<200)
                 {
                     frameCount++;
                     return;
                 }
                 
+                // Write photo to file
                 File file = new File(fileName);
                 try
                 {
+	                if(!file.exists())
+	                {
+	            		if(!file.mkdirs())
+	            			System.err.println("Failed to make required directories.");
+						file.createNewFile();
+	                }
                     ImageIO.write(ImageUtilities.createBufferedImageForDisplay(frame), "jpg", file);
                 }
                 catch (IOException e)
@@ -87,9 +123,13 @@ public class CakeCam
                 
                 //Define allowed QR codes
                 HashSet<String> FOOD = new HashSet<String>();
-                BufferedReader filereader;
+                for(String F : ccProperties.getProperty("cakecam.FOOD").split(","))
+                {
+                	FOOD.add(F.trim());
+                }
+                /*BufferedReader filereader;
                 try {
-                    filereader = new BufferedReader(new FileReader(PATH+"cakecamcodes.txt"));
+                    filereader = new BufferedReader(new FileReader("cakecamcodes.txt"));
                 
                     while(filereader.ready())
                     {
@@ -109,7 +149,7 @@ public class CakeCam
                 {
                     System.err.println("IO error: "+e.getMessage());
                     //e.printStackTrace();
-                }
+                }*/
 
                 //Create default subject
                 String food = "cake (or other food)";
@@ -170,12 +210,14 @@ public class CakeCam
                 {
                     System.err.println("No QR codes found: "+e1.getMessage());
                     //e1.printStackTrace();
-                } 
+                }
+                
+                String location = ccProperties.getProperty("cakecam.location", "regular food location");
 
                 Properties properties = System.getProperties();
 
                 // Setup mail server
-                properties.setProperty("mail.smtp.host", "smtp.ecs.soton.ac.uk");
+                properties.setProperty("mail.smtp.host", ccProperties.getProperty("mail.smtp.host"));
 
                 // Get the default Session object.
                 Session session = Session.getDefaultInstance(properties);
@@ -184,16 +226,16 @@ public class CakeCam
                 {
                     MimeMessage message = new MimeMessage(session);
 
-                    message.setFrom(new InternetAddress("tb12g09@ecs.soton.ac.uk", "CakeCam"));
+                    message.setFrom(new InternetAddress(ccProperties.getProperty("mail.from.address"), ccProperties.getProperty("mail.from.name")));
                      
-                    message.addRecipient(Message.RecipientType.TO, new InternetAddress("tb12g09@ecs.soton.ac.uk"));
+                    message.addRecipient(Message.RecipientType.TO, new InternetAddress(ccProperties.getProperty("mail.to.address")));
 
-                    message.setSubject("[CakeCam] - "+food+" in Building 32 coffee room");
+                    message.setSubject("[CakeCam] - "+food+" in "+location);
 
                     BodyPart messageBodyPart = new MimeBodyPart();
 
                     // Fill the message
-                    messageBodyPart.setText("This "+food+" can be found in the building 32 coffee room.");
+                    messageBodyPart.setText("This "+food+" can be found in the "+location+".");
 
                     Multipart multipart = new MimeMultipart();
                     multipart.addBodyPart(messageBodyPart);
@@ -211,6 +253,15 @@ public class CakeCam
 
                     Transport.send(message);
                     System.out.println("Sent message successfully....");
+                    
+                    postToTwitter();
+            		Twitter twitter = new TwitterFactory().getInstance();
+            		twitter.updateProfileImage(file);
+            		StatusUpdate status = new StatusUpdate("This "+food+" can be found in the "+location+"!");
+            		status.setMedia(file);
+            		twitter.updateStatus(status);
+            		
+            		System.out.println("Tweet posted successfully....");
                 }
                 catch (MessagingException mex)
                 {
@@ -221,6 +272,10 @@ public class CakeCam
                     System.err.println("Encoding error: "+e.getMessage());
                     e.printStackTrace();
                 }
+				catch (TwitterException e)
+				{
+					e.printStackTrace();
+				}
                 
                 System.exit(0);
             }
@@ -233,4 +288,8 @@ public class CakeCam
         };
         vd.addVideoListener(listener);
     }
+
+	protected static void postToTwitter()
+	{
+	}
 }
